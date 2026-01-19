271d35626d03952d7bf33a47421510837de07a41
Brook Li <emcdynamicist@gmail.com>
2026-01-19 17:58:02 +0900

    Initial working version for testing React Bootstrap Data Grid

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  sassOptions: {
+    silenceDeprecations: ["import"],
+    quietDeps: true,
+  },
 };
 
 export default nextConfig;
---
+++ src/app/CombinedGrid.tsx
@@ -0,0 +1,141 @@
+"use client";
+
+import Grid, {
+    ColDef,
+    EditableTableFilterState,
+    FilterModel,
+    GridPaginationState,
+    RowDef, SingleSelectModel, SortColDef, TableSortModel
+} from "@absreim/react-bootstrap-data-grid";
+import {FC, useMemo, useState} from "react";
+
+const cols: ColDef[] = [
+    {
+        type: "number",
+        name: "number",
+        label: "Number",
+        sortable: true,
+    },
+    {
+        type: "string",
+        name: "version",
+        label: "Version",
+    },
+    {
+        type: "date",
+        name: "date",
+        label: "Date",
+        sortable: true,
+    },
+];
+
+const rows: RowDef[] = [
+    {
+        number: 8,
+        version: "4.1.1.6758295",
+        date: new Date("2025-04-15"),
+    },
+    {
+        number: 7,
+        version: "4.1.1.5849914",
+        date: new Date("2024-09-05"),
+    },
+    {
+        number: 6,
+        version: "4.1.1.4763283",
+        date: new Date("2024-02-16"),
+    },
+    {
+        number: 5,
+        version: "4.1.1.4061076",
+        date: new Date("2023-11-30"),
+    },
+    {
+        number: 4,
+        version: "4.1.1.3882084",
+        date: new Date("2023-11-02"),
+    },
+    {
+        number: 3,
+        version: "4.1.1.3732833",
+        date: new Date("2023-09-22"),
+    },
+    {
+        number: 2,
+        version: "4.1.1.3686210",
+        date: new Date("2023-08-31"),
+    },
+    {
+        number: 1,
+        version: "4.1.1.3669438",
+        date: new Date("2023-08-25"),
+    },
+    {
+        number: 0,
+        version: "4.1.1.3622274",
+        date: new Date("2023-08-03"),
+    },
+];
+
+const CombinedGrid: FC = () => {
+    const [tableFilterState, setTableFilterState] =
+        useState<EditableTableFilterState>({
+            number: {
+                type: "number",
+                scheme: "lessOrEqual",
+                numValue: 7,
+                enabled: true,
+            },
+            version: {
+                type: "string",
+                scheme: "startsWith",
+                searchString: "4.1.1.3",
+                enabled: false,
+            },
+            date: {
+                type: "date",
+                scheme: "startFrom",
+                startDate: new Date("2023-08-15"),
+                enabled: true,
+            },
+        });
+
+    const filterModel: FilterModel = {tableFilterState, setTableFilterState};
+
+    const [pageSizeIndex, setPageSizeIndex] = useState(0)
+    const [pageNum, setPageNum] = useState(1)
+
+    const paginationState: GridPaginationState = {
+        pageSizeOptions: [5, 10],
+        pageSizeIndex: pageSizeIndex,
+        setPageSizeIndex: (pageSizeIndex) => setPageSizeIndex(pageSizeIndex),
+        currentPage: pageNum,
+        setCurrentPage: (pageNum) => setPageNum(pageNum),
+        maxPageButtons: 5,
+        componentSize: "medium"
+    }
+
+    const [sortColDef, setSortColDef] = useState<SortColDef | null>({
+        name: "number",
+        order: "desc",
+    });
+
+    const tableSortModel: TableSortModel = {
+        sortColDef,
+        setSortColDef,
+    };
+
+    const [selected, setSelected] = useState<number | null>(null);
+    const selectModel: SingleSelectModel = useMemo(() => ({
+        mode: "both",
+        type: "single",
+        selected,
+        setSelected,
+        groupName: "single selection example grid (BG3 cantrips)"
+    }), [selected]);
+
+    return <Grid rows={rows} cols={cols} filterModel={filterModel} pagination={paginationState}
+                 sortModel={tableSortModel} selectModel={selectModel}/>;
+};
+
+export default CombinedGrid;
